60cb22eb6eb502da4fda7249da7e52a56939d959
Walter Cheng <walter.cheng@carleton.ca>
2017-10-27 17:20:11 -0400

    Add files via upload

--- fly.h
+++ fly.h
@@ -1,5 +1,5 @@
-#ifndef SCENE_NODE_H_
-#define SCENE_NODE_H_
+#ifndef FLY_H
+#define FLY_H
 
 #include <string>
 #define GLEW_STATIC
@@ -11,68 +11,17 @@
 
 #include "resource.h"
 #include "camera.h"
+#include "scene_node.h"
 
-namespace game {
+namespace game 
+{
 
-    // Class that manages one object in a scene 
-    class Fly {
-		/*
-        public:
-            // Create scene node from given resources
-            SceneNode(const std::string name, const Resource *geometry, const Resource *material, const Resource *texture = NULL);
+    class Fly : public SceneNode
+	{
+		
 
-            // Destructor
-            ~SceneNode();
-            
-            // Get name of node
-            const std::string GetName(void) const;
-
-            // Get node attributes
-            glm::vec3 GetPosition(void) const;
-            glm::quat GetOrientation(void) const;
-            glm::vec3 GetScale(void) const;
-
-            // Set node attributes
-            void SetPosition(glm::vec3 position);
-            void SetOrientation(glm::quat orientation);
-            void SetScale(glm::vec3 scale);
-            
-            // Perform transformations on node
-            void Translate(glm::vec3 trans);
-            void Rotate(glm::quat rot);
-            void Scale(glm::vec3 scale);
-
-            // Draw the node according to scene parameters in 'camera'
-            // variable
-            virtual void Draw(Camera *camera);
-
-            // Update the node
-            virtual void Update(void);
-
-            // OpenGL variables
-            GLenum GetMode(void) const;
-            GLuint GetArrayBuffer(void) const;
-            GLuint GetElementArrayBuffer(void) const;
-            GLsizei GetSize(void) const;
-            GLuint GetMaterial(void) const;
-
-        private:
-            std::string name_; // Name of the scene node
-            GLuint array_buffer_; // References to geometry: vertex and array buffers
-            GLuint element_array_buffer_;
-            GLenum mode_; // Type of geometry
-            GLsizei size_; // Number of primitives in geometry
-            GLuint material_; // Reference to shader program
-            GLuint texture_; // Reference to texture resource
-            glm::vec3 position_; // Position of node
-            glm::quat orientation_; // Orientation of node
-            glm::vec3 scale_; // Scale of node
-
-            // Set matrices that transform the node in a shader program
-            void SetupShader(GLuint program);
-			*/
-    }; // class SceneNode
+    }; 
 
 } // namespace game
 
-#endif // SCENE_NODE_H_
+#endif //FLY_H
